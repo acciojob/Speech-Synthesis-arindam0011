fb647ff0f68f3fde2beb8a92d92ec60b5ab624ce
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-21 02:08:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Your script here.
+
+
+window.speechSynthesis.onvoiceschanged = function() {
+const voicearea=document.getElementById("voices");
+  let voices = window.speechSynthesis.getVoices();
+  voices.forEach(voice => {
+    let option = document.createElement('option');
+    option.textContent = voice.name;
+    voicearea.appendChild(option);
+  });
+};
